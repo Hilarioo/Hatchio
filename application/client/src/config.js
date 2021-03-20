ad2385c81c8cf596bd8b66d5a33b8db4e94f21ec
rolandlee0308 <rolandlee500@gmail.com>
2021-03-20 08:25:26 -0700

    Start fresh from aaron-branch

--- application/client/src/config.js
+++ application/client/src/config.js
@@ -4,7 +4,7 @@ const local_port = `http://localhost:5000`;
 const dev_environment = (localmachine) => {
   return localmachine ? local_port : aws_port;
 };
-const port_host = dev_environment(false);
+const port_host = dev_environment(true);
 
 module.exports = {
   port_host,
