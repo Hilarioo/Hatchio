040edf2d4ce55870a28c59400baa4438ec7bc446
singhaaron <singhaaron191@gmail.com>
2021-04-05 22:33:37 -0700

    ratings_template

--- frontend/src/views/js/rating/Ratings.js
+++ frontend/src/views/js/rating/Ratings.js
@@ -3,14 +3,92 @@ import { useState, useEffect } from "react";
 import StudentCard from "./RatingCard.js";
 //import API
 import API_FETCH_STUDENT from "../../../models/student_cards";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
 // CSS
 import "../../css/Theme.css";
 import "../../css/Search.css";
 const Ratings = () => {
   const [dbStudents, setdbStudents] = useState([]);
   useEffect(() => API_FETCH_STUDENT(setdbStudents, []));
+  const handleSubmit = (e) => {
+    //insert into associate student ratings page
+  };
   return (
     <div>
+      <h4>pop up form to be implemented</h4>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Recommendation </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            placeholder="String text with words that assess students in generic categories like his/her honesty,integrity,courage,skillset,etc."
+            aria-label="text"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Responsible </InputGroup.Text>
+          </InputGroup.Prepend>
+
+          <Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+            <option value="Select">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </Form.Control>
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">TeamWork </InputGroup.Text>
+          </InputGroup.Prepend>
+
+          <Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+            <option value="Select">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </Form.Control>
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Leadership </InputGroup.Text>
+          </InputGroup.Prepend>
+
+          <Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+            <option value="Select">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </Form.Control>
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Committed to Success </InputGroup.Text>
+          </InputGroup.Prepend>
+
+          <Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+            <option value="Select">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </Form.Control>
+        </InputGroup>
+        <Button variant="dark" type="submit">
+          Rate Student
+        </Button>
+      </Form>
       <div class="results">
         {dbStudents.map((student) => (
           <StudentCard
