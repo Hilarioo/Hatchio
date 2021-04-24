254e60f6ba7f3bb81a080e971ff299772721c9ec
singhaaron <singhaaron191@gmail.com>
2021-04-23 19:39:50 -0700

    temp job insertion fix

--- application/backend/routes/insert.js
+++ application/backend/routes/insert.js
@@ -4,21 +4,25 @@
  * Functionality IE: Frontend Employer's Dashboard to Post Jobs
  * Author: Aaron, Lyra
  */
-const SQL_CONNECTION = require('../config').SQL_CONNECTION;
+const SQL_CONNECTION = require("../config").SQL_CONNECTION;
 
 module.exports = (app) => {
-  var insertRoute = function(url, query, jsonKeys) {
+  var insertRoute = function (url, query, jsonKeys) {
     app.post(url, (req, res) => {
       try {
-        SQL_CONNECTION.query(query, jsonKeys.map(key => req.body[key]), (err, results) => {
-          if (err) {
-            //Send 400: Bad Request
-            return res.sendStatus(400);
-          } else {
-            //Send 201: Request Created
-            return res.sendStatus(200);
+        SQL_CONNECTION.query(
+          query,
+          jsonKeys.map((key) => req.body[key]),
+          (err, results) => {
+            if (err) {
+              //Send 400: Bad Request
+              return res.sendStatus(400);
+            } else {
+              //Send 201: Request Created
+              return res.sendStatus(200);
+            }
           }
-        });
+        );
       } catch (e) {
         console.log(`Error ${e}`);
         return res.sendStatus(400);
@@ -27,6 +31,7 @@ module.exports = (app) => {
   };
   // TODO: many of these categories should be domain-limited
   //POST: Job Listing From Employer
+  /*
   insertRoute('/insert_job', `insert into company_listings(employer_id,
                                                            organization_name,
                                                            position_title,
@@ -42,16 +47,45 @@ module.exports = (app) => {
                                                            task_responsibilities) values (?,?,?,?,?,?,?,?,?,?,?,?,?);`,
     ['Employer_ID', 'organization_name', 'position_title', 'location', 'job_type', 'experience_years',
       'salary', 'about_us', 'the_opportunity', 'skillset', 'benefits', 'task_responsibilities']);
+      */
   //POST: Student Profile Page
-  insertRoute('/insert_student_profile_page',
-              'insert into student_profile_page(student_id,about_me,strengths_qualities,location,school_grade_level) values (?,?,?,?,?);',
-              ['Student_ID', 'about_me', 'strengths_qualities', 'location', 'school_grade_level']);
+  insertRoute(
+    "/insert_student_profile_page",
+    "insert into student_profile_page(student_id,about_me,strengths_qualities,location,school_grade_level) values (?,?,?,?,?);",
+    [
+      "Student_ID",
+      "about_me",
+      "strengths_qualities",
+      "location",
+      "school_grade_level",
+    ]
+  );
   //POST Methods for Student Education
-  insertRoute('/insert_student_education',
-              'insert into student_education(student_id, school, degree, school_gpa, study_major, start_year, end_year) values (?,?,?,?,?,?,?);',
-              ['Student_ID', 'school', 'degree', 'school_gpa', 'study_major', 'start_year', 'end_year']);
+  insertRoute(
+    "/insert_student_education",
+    "insert into student_education(student_id, school, degree, school_gpa, study_major, start_year, end_year) values (?,?,?,?,?,?,?);",
+    [
+      "Student_ID",
+      "school",
+      "degree",
+      "school_gpa",
+      "study_major",
+      "start_year",
+      "end_year",
+    ]
+  );
   //POST Methods for Student Projects
-  insertRoute('/insert_student_projects',
-              'insert into student_projects(student_id, project_name, summary, arr_tools_used, professor, links_website, arr_collaborators_arr) values (?,?,?,?,?,?,?);',
-              ['Student_ID', 'project_name', 'summary', 'arr_tools_used', 'arr_tools_used' /* TODO I assume this should be professor */, 'links_website', 'arr_collaborators_arr']);
+  insertRoute(
+    "/insert_student_projects",
+    "insert into student_projects(student_id, project_name, summary, arr_tools_used, professor, links_website, arr_collaborators_arr) values (?,?,?,?,?,?,?);",
+    [
+      "Student_ID",
+      "project_name",
+      "summary",
+      "arr_tools_used",
+      "arr_tools_used" /* TODO I assume this should be professor */,
+      "links_website",
+      "arr_collaborators_arr",
+    ]
+  );
 };
--- application/backend/routes/tmp.js
+++ application/backend/routes/tmp.js
@@ -29,4 +29,22 @@ module.exports = (app) => {
       return res.sendStatus(400);
     }
   });
+  //POST: Job Listing From Employer
+  app.post("/insert_job", (req, res) => {
+    try {
+      let sql_insert_job_listings = `insert into company_listings(employer_id,organization_name,position_title,location,job_type,experience_years,experience_level,salary,about_us,the_opportunity,skillset,benefits,task_responsibilities) values (${req.body.Employer_ID},"${req.body.organization_name}","${req.body.position_title}","${req.body.location}","${req.body.job_type}",${req.body.experience_years},"${req.body.experience_levels}",${req.body.salary},"${req.body.about_us}","${req.body.the_opportunity}","${req.body.skillset}","${req.body.benefits}","${req.body.task_responsibilities}");`;
+      SQL_CONNECTION.query(sql_insert_job_listings, (err, results) => {
+        if (err) {
+          //Send 400: Bad Request
+          return res.sendStatus(400);
+        } else {
+          //Send 201: Request Created
+          return res.sendStatus(200);
+        }
+      });
+    } catch (e) {
+      console.log(`Error ${e}`);
+      return res.sendStatus(400);
+    }
+  });
 };
