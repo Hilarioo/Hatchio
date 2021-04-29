96494bf32be6d8c920e41f57b9de056663b1dee8
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-04-29 06:55:08 -0700

    cleaned job card JS File

--- application/frontend/src/views/js/search/jobs/JobCard.js
+++ application/frontend/src/views/js/search/jobs/JobCard.js
@@ -40,63 +40,43 @@ const JobCard = ({
     });
   };
 
-  if (cookie.Type_User === `student`) {
-    return (
-      <div className='card-result'>
-        <header>
-          <img
-            src={image.length <= 0 ? defaultImage(CompanyName) : image}
-            alt={CompanyName.charAt(0)}
-          />
-          <div className='info'>
-            <div className='name-enrollment'>
-              <h4 style={{ paddingBottom: "10px" }}>{PositionTitle}</h4>
-            </div>
-
-            <div className='flex-box'>
-              <h6 style={{ marginRight: "0.5rem" }}>{CompanyName}</h6>
-              <p id='enrollment' style={{ marginTop: "-.2rem" }}>
-                {JobType}
-              </p>
-              <p id='salary'>${String(Income).slice(0, -3)}k / yr</p>
-            </div>
-            <div className='flext-box' style={{ marginTop: "-.7rem" }}>
-              <img src={LocationIcon} alt='location pin' id='location' />
-              <span>{Location}</span>
-            </div>
-          </div>
-        </header>
-        <div className='flex-box'>
-          <Button onClick={() => Redirect_Job_View(Listing_ID)}>View</Button>
-          <Popup trigger={<Button>Apply</Button>}>
-            <p>Sent!(Aaron working on) </p>
-          </Popup>
-        </div>
-      </div>
-    );
-  }
   return (
     <div className='card-result'>
-      <img src={circle} alt='' />
-      <h4>
-        <b>Position Title:</b> {PositionTitle}
-      </h4>
-      <p>
-        <b>Company Name:</b> {CompanyName}
-      </p>
-      <p>
-        <b>Income:</b> {Income}
-      </p>
-      <p>
-        <b>Job Type:</b> {JobType}
-      </p>
-      <p>
-        <b>About Us:</b> {AboutUs}
-      </p>
-      <Button onClick={() => Redirect_Job_View(Listing_ID)}>View</Button>
-      <Popup trigger={<Button>Apply</Button>}>
-        <p>Sign In as a student! </p>
-      </Popup>
+      <header>
+        <img
+          src={image.length <= 0 ? defaultImage(CompanyName) : image}
+          alt={CompanyName.charAt(0)}
+        />
+        <div className='info'>
+          <div className='name-enrollment'>
+            <h4 style={{ paddingBottom: "10px" }}>{PositionTitle}</h4>
+          </div>
+
+          <div className='flex-box'>
+            <h6 style={{ marginRight: "0.5rem" }}>{CompanyName}</h6>
+            <p id='enrollment' style={{ marginTop: "-.2rem" }}>
+              {JobType}
+            </p>
+            <p id='salary'>${String(Income).slice(0, -3)}k / yr</p>
+          </div>
+          <div className='flext-box' style={{ marginTop: "-.7rem" }}>
+            <img src={LocationIcon} alt='location pin' id='location' />
+            <span>{Location}</span>
+          </div>
+        </div>
+      </header>
+      <div className='flex-box'>
+        <Button onClick={() => Redirect_Job_View(Listing_ID)}>View</Button>
+        <Popup trigger={<Button>Apply</Button>}>
+          {cookie.Type_User === "student" ||
+          cookie.Type_User === "employer" ||
+          cookie.Type_User === "professor" ? (
+            <p> Sent!(Aaron working on) </p>
+          ) : (
+            <p> Sign In to apply! </p>
+          )}
+        </Popup>
+      </div>
     </div>
   );
 };
