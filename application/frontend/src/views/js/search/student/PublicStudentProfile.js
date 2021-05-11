ad952cc688ec8c8bef670f6e6e70364d8b37786c
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-05-11 10:48:51 -0700

    removed URL links from public profiles

--- application/frontend/src/views/js/search/student/PublicStudentProfile.js
+++ application/frontend/src/views/js/search/student/PublicStudentProfile.js
@@ -13,10 +13,6 @@ import Button from "react-bootstrap/Button";
 import ProgressBar from "react-bootstrap/ProgressBar";
 //Import SVG Icons
 import LocationIcon from "../../../content/svg/location-icon.svg";
-import GlobeIcon from "../../../content/svg/globe-icon.svg";
-import LinkedinIcon from "../../../content/svg/linkedin-icon.svg";
-import ResumeIcon from "../../../content/svg/resume-icon.svg";
-import GithubIcon from "../../../content/svg/github-icon.svg";
 import ProjectIcon from "../../../content/svg/project-icon.svg";
 import ExperienceIcon from "../../../content/svg/experience-icon.svg";
 import EducationIcon from "../../../content/svg/education-icon.svg";
@@ -64,27 +60,39 @@ const PublicStudentProfile = ({
   return (
     <>
       {/* heading */}
-      <div className="student-heading">
+      <div className='student-heading'>
         {/* creates default image if none provided */}
         <img
-          src={userProfile[1][0].profile_image === null ? defaultImage(studentName) : userProfile[1][0].profile_image}
-          alt={"S"}
+          src={
+            userProfile[1][0].profile_image === null
+              ? defaultImage(studentName)
+              : userProfile[1][0].profile_image
+          }
+          alt={studentName}
         />
-        <div className="right">
+        <div className='right'>
           {/* Student Name */}
           <h1>{studentName}</h1>
 
           {/* Student Location */}
-          <div className="flex-box">
-            <img src={LocationIcon} alt="location pin" />
+          <div className='flex-box'>
+            <img src={LocationIcon} alt='location pin' />
             <p>{userProfile[1][0].location}</p>
           </div>
-          <div className="flex-box">
+          <div className='flex-box'>
             {/* Only visible to employers */}
-            {cookie.Type_User === "employer" ? <Button onClick={() => setHirePopup(true)}>Hire</Button> : null}
-            <HirePopup show={hirePopup} onHide={() => setHirePopup(false)} setHirePopup={setHirePopup} />
+            {cookie.Type_User === "employer" ? (
+              <Button onClick={() => setHirePopup(true)}>Hire</Button>
+            ) : null}
+            <HirePopup
+              show={hirePopup}
+              onHide={() => setHirePopup(false)}
+              setHirePopup={setHirePopup}
+            />
             {/* Only visible to professors */}
-            {cookie.Type_User === "professor" ? <Button onClick={() => setRatingPopup(true)}>Rate</Button> : null}
+            {cookie.Type_User === "professor" ? (
+              <Button onClick={() => setRatingPopup(true)}>Rate</Button>
+            ) : null}
             <RatingPopup
               show={ratingPopup}
               onHide={() => setRatingPopup(false)}
@@ -96,30 +104,26 @@ const PublicStudentProfile = ({
         </div>
       </div>
 
-      {/* TODO:: Links */}
-      <div className="flex-box student-links" style={{ justifyContent: "space-between" }}>
-        <img src={GlobeIcon} alt="website url" />
-        <img src={GithubIcon} alt="github" />
-        <img src={LinkedinIcon} alt="linkedin" />
-        <img src={ResumeIcon} alt="resume-pdf" />
-      </div>
-
       {/* Student About Me */}
-      <div className="student-about">
-        <div className="flex-box">
+      <div className='student-about'>
+        <div className='flex-box'>
           <h4>About Me</h4>
         </div>
         {/* About Me */}
         <p>{userProfile[1][0].about_me}</p>
       </div>
       {/* Student Qualities */}
-      <div className="student-qualities">
-        <div className="flex-box">
+      <div className='student-qualities'>
+        <div className='flex-box'>
           <h4>Top Qualities</h4>
         </div>
         {/* Maps Every Quality Stored For The Student */}
         <li>
-          {String(userProfile[1].length === 0 ? "No Qualtites" : userProfile[1][0].strengths_qualities)
+          {String(
+            userProfile[1].length === 0
+              ? "No Qualtites"
+              : userProfile[1][0].strengths_qualities
+          )
             .split(",")
             .map((quality) => (
               <ul>{quality}</ul>
@@ -127,30 +131,30 @@ const PublicStudentProfile = ({
         </li>
       </div>
 
-      <div className="student-grid">
+      <div className='student-grid'>
         {/* Student Projects */}
-        <div className="projects">
-          <div className="flex-box">
+        <div className='projects'>
+          <div className='flex-box'>
             <h4>Projects</h4>
           </div>
           {/* Maps Every Project Stored For The Student */}
           {userProfile[2].map((project) => (
-            <div className="student-project flex-box">
+            <div className='student-project flex-box'>
               {/* Project Icon */}
-              <div className="img-box">
-                <img src={ProjectIcon} alt="project icon" />
+              <div className='img-box'>
+                <img src={ProjectIcon} alt='project icon' />
               </div>
               {/* Project Details */}
-              <div className="right">
+              <div className='right'>
                 {/* TODO:: Project Date */}
-                <p id="date">November 20, 2020</p>
-                <div className="flex-box">
+                <p id='date'>November 20, 2020</p>
+                <div className='flex-box'>
                   {/* Project Name */}
                   <h5>{project.project_name}</h5>
                 </div>
                 {/* Project Description */}
                 <p>{project.summary}</p>
-                <div className="flex-box">
+                <div className='flex-box'>
                   {/* Project Collaborator(s) */}
                   <h6>Collaborator(s):</h6>
                   {String(project.arr_collaborators_arr)
@@ -174,20 +178,20 @@ const PublicStudentProfile = ({
         </div>
 
         <div>
-          <div className="education">
+          <div className='education'>
             {/* Student Education */}
-            <div className="flex-box">
+            <div className='flex-box'>
               <h4>Education</h4>
             </div>
             {/* Maps Every Education The Student Has Stored */}
             {userProfile[0].map((education) => (
-              <div className="student-education flex-box">
+              <div className='student-education flex-box'>
                 {/* creates default image if none provided */}
-                <div className="img-box">
-                  <img src={EducationIcon} alt="project icon" />
+                <div className='img-box'>
+                  <img src={EducationIcon} alt='project icon' />
                 </div>
-                <div className="right">
-                  <div className="flex-box">
+                <div className='right'>
+                  <div className='flex-box'>
                     {/* Education Degree Recieved */}
                     <h5>{education.degree}</h5>
                   </div>
@@ -202,31 +206,34 @@ const PublicStudentProfile = ({
               </div>
             ))}
           </div>
-          <div className="experience">
+          <div className='experience'>
             {/* experience */}
 
-            <div className="flex-box">
-              <h4 className="category-heading">Experience</h4>
+            <div className='flex-box'>
+              <h4 className='category-heading'>Experience</h4>
             </div>
             {/** Map Experience */}
             {userProfile[5].length === 0
               ? "No Experience"
               : userProfile[5].map((data) => (
-                  <div className="student-experience flex-box">
+                  <div className='student-experience flex-box'>
                     {/* creates default image if none provided */}
-                    <div className="img-box">
-                      <img src={ExperienceIcon} alt="project icon" />
+                    <div className='img-box'>
+                      <img src={ExperienceIcon} alt='project icon' />
                     </div>
-                    <div className="right">
-                      <div className="flex-box">
+                    <div className='right'>
+                      <div className='flex-box'>
                         <h5>{data.experience_title_position}</h5>
                       </div>
                       <h6>{data.company_name}</h6>
                       <p>
-                        {converter(data.date_start)} - {converter(data.date_end)}
+                        {converter(data.date_start)} -{" "}
+                        {converter(data.date_end)}
                       </p>
                       <li>
-                        <ul style={{ backgroundColor: "#EFE271" }}>{data.arr_work_done_keywords}</ul>
+                        <ul style={{ backgroundColor: "#EFE271" }}>
+                          {data.arr_work_done_keywords}
+                        </ul>
                       </li>
                     </div>
                   </div>
@@ -235,13 +242,13 @@ const PublicStudentProfile = ({
         </div>
       </div>
       {/* Student's Ratings */}
-      <div className="student-reflection">
+      <div className='student-reflection'>
         <h4>Ratings</h4>
         {/* Maps Every Review From Professor(s) to Student */}
         {userProfile[3].map((review) => (
-          <div className="rating">
-            <header className="flex-box">
-              <span className="flex-box">
+          <div className='rating'>
+            <header className='flex-box'>
+              <span className='flex-box'>
                 {/* Review: Professor Name */}
                 <h5>
                   {review.first_name} {review.last_name}
@@ -250,11 +257,11 @@ const PublicStudentProfile = ({
                 <ProgressBar
                   now={review.rating_total}
                   label={`${review.rating_total}` + " / 5"}
-                  min="0"
-                  max="5"
-                  variant="info"
+                  min='0'
+                  max='5'
+                  variant='info'
                   style={{ width: "35%", marginTop: "5px" }}
-                  id="progress-bar"
+                  id='progress-bar'
                 />
               </span>
               {/* TODO:: date of the review */}
